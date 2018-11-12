7ca10162b4ded6371926cec3ae68cea567a946b8
xiam <xiam@gzsendi.cn>
2018-11-12 16:22:51 +0800

    heh

--- src/com/jiaozi/thread/MainTest.java
+++ src/com/jiaozi/thread/MainTest.java
@@ -1,7 +1,7 @@
 package com.jiaozi.thread;
 
 public class MainTest {
-    //hehehl
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("hello!world!");
